48c1b4d80a580fc872e256076fc528290141cfa2
awolfafterdark <32263318+awolfafterdark@users.noreply.github.com>
2023-03-11 17:12:14 -0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,17 +1,10 @@
-#!/usr/bin/env bash
-#enable developer mode on mac to fix palera1n app install
-
-
-echo ""
-#enable developer mode on mac
-echo "Enabling developer mode on mac..."
-DevToolsSecurity -enable
-echo "Mac Developer mode done!"
-echo ""
-echo "Restart USB connection to fix random connection on/off bug...."
-killall -STOP -c usbd
-echo "Killed USB connection. Restarting..."
-echo "Done."
-echo ""
-exit 1
-
+@echo off
+chcp 65001
+echo Enabling developer mode on Windows...
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1" 2>&1 >nul
+echo Windows Developer mode done!
+echo Restarting USB connection to fix random connection on/off bug...
+net stop "usbaap" 2>&1 >nul
+net start "usbaap" 2>&1 >nul
+echo USB connection restarted. Done.
+exit /b 0
